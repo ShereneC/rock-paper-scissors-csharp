7e05856e109670fff3a85c7cafcdaa87fb33669c
ShereneC <spyhopper@cableone.net>
2021-08-30 16:12:45 -0600

    Made Game class

--- Models/Game.cs
+++ Models/Game.cs
@@ -1,13 +1,72 @@
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-// namespace rock_paper_scissors_csharp  
-// {
+namespace rock_paper_scissors_csharp  
+{
 
-// public class Game
-// {
-// public string<>
-// } 
+public class Game
+{
+            string rock = (@"
+        
+    **  
+  ****  
+ ****** 
+      ");
+
+            string paper = (@"
+        
+ ******  
+ ****** 
+ ****** 
+      ");
+
+            string scissors = (@"
+  * * 
+   *   
+  * * 
+ *   * 
+      ");
+
+      public void Play(){
 
 
-// }
+        Random random = new Random();
+        int rnd = random.Next(1, 4);
+
+            Console.Clear();
+            Console.WriteLine("Would you like to play Rock Paper Scissors? (y/n)");
+            var choice = Console.ReadKey().KeyChar;
+            if (choice == 'n')
+            {
+              Console.WriteLine("\nOkay, well I don't understand why you came here. Goodbye");
+            }
+            else
+            {
+              Console.WriteLine("Let's Play!");
+              Console.WriteLine("\nChoose your weapon...  (Rock = r, Paper = p, Scissors = s");
+              var weapon = Console.ReadKey().KeyChar;
+              if ((weapon == 'r' && rnd == 3) || (weapon == 'p' && rnd == 1) || (weapon == 's' && rnd ==2))
+              {
+              Console.WriteLine("Computer chose: " + rnd);
+              Console.WriteLine("You chose: " + weapon);
+              Console.WriteLine("You Win!");
+              }
+              else if ((weapon == 'r' && rnd == 1) || (weapon == 'p' && rnd == 2) || (weapon == 's' && rnd ==3))
+               {
+              Console.WriteLine("Computer chose: " + rnd);
+              Console.WriteLine("You chose: " + weapon);
+              Console.WriteLine("Draw!");
+              }
+              else 
+              {
+              Console.WriteLine("Computer chose: " + rnd);
+              Console.WriteLine("You chose: " + weapon);
+              Console.WriteLine("You Lose!");
+              }
+            }
+      }
+} 
+
+
+}
 
--- Program.cs
+++ Program.cs
@@ -6,62 +6,10 @@ namespace rock_paper_scissors_csharp
     {
         static void Main(string[] args)
         {
+Console.Clear();
+Game game = new Game();
+game.Play();
 
-            string rock = (@"
-        
-    **  
-  ****  
- ****** 
-      ");
-
-            string paper = (@"
-        
- ******  
- ****** 
- ****** 
-      ");
-
-            string scissors = (@"
-  * * 
-   *   
-  * * 
- *   * 
-      ");
-
-Random random = new Random();
-int rnd = random.Next(1, 4);
-
-            Console.Clear();
-            Console.WriteLine("Would you like to play Rock Paper Scissors? (y/n)");
-            var choice = Console.ReadKey().KeyChar;
-            if (choice == 'n')
-            {
-              Console.WriteLine("\nOkay, well I don't understand why you came here. Goodbye");
-            }
-            else
-            {
-              Console.WriteLine("Let's Play!");
-              Console.WriteLine("\nChoose your weapon...  (Rock = r, Paper = p, Scissors = s");
-              var weapon = Console.ReadKey().KeyChar;
-              if ((weapon == 'r' && rnd == 3) || (weapon == 'p' && rnd == 1) || (weapon == 's' && rnd ==2))
-              {
-              Console.WriteLine("Computer chose: " + rnd);
-              Console.WriteLine("You chose: " + weapon);
-              Console.WriteLine("You Win!");
-              }
-              else if ((weapon == 'r' && rnd == 1) || (weapon == 'p' && rnd == 2) || (weapon == 's' && rnd ==3))
-               {
-              Console.WriteLine("Computer chose: " + rnd);
-              Console.WriteLine("You chose: " + weapon);
-              Console.WriteLine("Draw!");
-              }
-              else 
-              {
-              Console.WriteLine("Computer chose: " + rnd);
-              Console.WriteLine("You chose: " + weapon);
-              Console.WriteLine("You Lose!");
-              }
-            }
         }
     }
 }
